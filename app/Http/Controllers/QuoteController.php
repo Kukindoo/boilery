6d4486f1c75eaee43c3884aadb258f6c3b297131
Jakub Zavacky <Jakub.Zavacky@gmail.com>
2026-09-14 15:05:33 +0200

    chore: added show

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RequestedQuotes;
+
 class QuoteController extends Controller
 {
     public function index()
     {
         return view('quotes.index');
+    }
+
+    public function show(RequestedQuotes $quote)
+    {
+        return view('quotes.show', [
+            'quote' => $quote,
+        ]);
     }
 }
