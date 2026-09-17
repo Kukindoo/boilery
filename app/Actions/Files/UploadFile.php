2df4f8921d6630c35ef433bee31becaa6b573aab
Jakub Zavacky <Jakub.Zavacky@gmail.com>
2026-09-17 08:43:40 +0200

    chore: added FileUpload.php action

---
+++ app/Actions/Files/UploadFile.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Actions\Files;
+
+use App\Enums\FileTypes;
+use App\Models\File;
+use App\Models\RequestedQuotes;
+use Illuminate\Support\Str;
+
+class UploadFile
+{
+    public function handle($file,
+        RequestedQuotes $quote,
+        FileTypes $fileType)
+    {
+        $path = $file->store('files');
+
+        $extension = $file->getClientOriginalExtension();
+
+        $fileNameDirty = implode('_', [
+            $quote->first_name,
+            $quote->last_name,
+            $quote->id,
+            $fileType->snake(),
+        ]);
+
+        $fileName = Str::ascii($fileNameDirty) . '.' . $extension;
+
+        return File::create([
+            'path' => $path,
+            'quote_id' => $quote->id,
+            'original_name' => $file->getClientOriginalName(),
+            'extension' => $extension,
+            'mime_type' => $file->getMimeType(),
+            'size' => $file->getSize(),
+            'label' => $fileName,
+        ]);
+    }
+}
