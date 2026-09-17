d6856a04954821eecb5051a44bdd0944c6804646
Jakub Zavacky <Jakub.Zavacky@gmail.com>
2026-09-17 15:17:29 +0200

    chore: password creation

--- database/migrations/2026_09_13_133052_add_base_user.php
+++ database/migrations/2026_09_13_133052_add_base_user.php
@@ -2,15 +2,16 @@
 
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
 
-
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         User::create([
             'name' => 'Radomír Závacký',
             'email' => 'bojlery@seznam.cz',
-            'password' => bcrypt('vjhdbkv$%@$@buofsd'),
+            'password' => bcrypt(Str::password()),
         ]);
     }
 };
